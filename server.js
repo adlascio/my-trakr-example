d21393fd89583401fb4bf0f07e3e563f7564b077
adlascio <arthur.dilascio@gmail.com>
2022-07-12 16:27:55 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,60 @@
+//create express server
+import express from 'express';
+import cors from 'cors';
+import { addAccount, getAccounts } from './src/accounts.js';
+import { addTransaction, getAllTransactions } from './src/transactions.js';
+import { addCategory, getCategories } from './src/categories.js';
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
+
+// read accounts
+app.get('/accounts', (req, res) => {
+  const accounts = getAccounts();
+  res.status(200).json(accounts);
+});
+
+// add new account
+app.post('/accounts', (req, res) => {
+  if (!req.body.newAccount) {
+    res.send('Invalid data');
+  }
+  const newAccount = addAccount(req.body.newAccount);
+  res.status(201).json(newAccount);
+});
+
+//get all transactions
+app.get('/transactions', (req, res) => {
+  const transactions = getAllTransactions();
+  res.status(200).json(transactions);
+});
+
+// add new transaction
+app.post('/transaction', (req, res) => {
+  if (!req.body.newTransaction) {
+    res.status(400).send('Invalid data');
+  }
+  const newTransactions = addTransaction(req.body.newTransaction);
+  res.status(201).json(newTransactions);
+});
+
+// read categories
+app.get('/categories', (req, res) => {
+  const categories = getCategories();
+  res.status(200).json(categories);
+});
+
+// add new category
+app.post('/categories', (req, res) => {
+  if (!req.body.newCategory) {
+    res.status(400).send('Invalid data');
+  }
+  const newCategory = addCategory(req.body.newCategory);
+  res.status(201).json(newCategory);
+});
---
+++ src/transactions.js
@@ -0,0 +1,37 @@
+import { getAccounts } from './accounts.js';
+
+let transactionCounter = 0;
+export const addTransaction = (transaction) => {
+  const newTransactions = [];
+  const accounts = getAccounts();
+  accounts.forEach((account) => {
+    if (
+      account.id == transaction.accountIdFrom ||
+      account.id == transaction.accountIdTo ||
+      account.id == transaction.accountId
+    ) {
+      transactionCounter++;
+      let newTransaction = { ...transaction, id: transactionCounter };
+      if (account.id == transaction.accountIdFrom) {
+        newTransaction.accountId = transaction.accountIdFrom;
+      }
+      if (account.id == transaction.accountIdTo) {
+        newTransaction.accountId = transaction.accountIdTo;
+      }
+      account.transactions.push(newTransaction);
+      newTransactions.push(newTransaction);
+    }
+  });
+  return newTransactions;
+};
+
+export const getAllTransactions = () => {
+  const accounts = getAccounts();
+  let allTransactions = [];
+  accounts.forEach((account) => {
+    allTransactions = [...allTransactions, ...account.transactions];
+  });
+  return allTransactions;
+};
+
+export default { addTransaction, getAllTransactions };
